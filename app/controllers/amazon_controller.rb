613221c3da8e0ee462c5b28436b3657e64f95ede
Ash Skutches <ash.skutches@docusign.com>
2016-03-01 21:34:32 -0800

    Let's try doing it all at once!

--- app/controllers/amazon_controller.rb
+++ app/controllers/amazon_controller.rb
@@ -1,11 +1,18 @@
 class AmazonController < ApplicationController
+  before_action :set_amazons
 
   def sync
-    amazon = Amazon.new
-    if amazon.sync
+    @mailchimp.delete_amazon_users_off_mailchimp
+    if @amazon.sync
+      @mailchimp.add_new_amazon_customers_to_mailchimp
       redirect_to '/customers', notice: 'Sync was successful!'
     else
       render :new, failure: "Something went wrong"
     end
+  end
+
+  def set_amazons
+    @amazon = Amazon.new
+    @mailchimp = AmazonMailChimp.new
   end
 end
